8f7a2dd3a5a55da30501bc89208062a9afb4e404
DBenAsuli <dvir971@gmail.com>
2024-06-06 14:19:28 +0300

    Code Cleanup

--- client.py
+++ client.py
@@ -1,6 +1,6 @@
-# Advanced Topics in Online Privacy and Cybersecurity                  Excercise 1
-# Dvir Ben Asuli                                                       318208816
-# The Hebrew University of Jerusalem                                   June 2024
+# Advanced Topics in Online Privacy and Cybersecurity     Excercise 1
+# Dvir Ben Asuli                                          318208816
+# The Hebrew University of Jerusalem                      June 2024
 
 class Client():
     def __init__(self):
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-# Advanced Topics in Online Privacy and Cybersecurity                  Excercise 1
-# Dvir Ben Asuli                                                       318208816
-# The Hebrew University of Jerusalem                                   June 2024
+# Advanced Topics in Online Privacy and Cybersecurity     Excercise 1
+# Dvir Ben Asuli                                          318208816
+# The Hebrew University of Jerusalem                      June 2024
 
 if __name__ == '__main__':
     pass
