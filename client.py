7f0e476c288415b9f128c33621afdf5bd319504c
DBenAsuli <dvir971@gmail.com>
2024-06-18 17:46:04 +0300

    Added more documentation

--- client.py
+++ client.py
@@ -17,38 +17,46 @@ class Client():
 
     # The client stores data associated with an ID on the server by calling:
     def store_data(self, server, id, data):
-        self.access(id, new_data=data)
+        self.access(id, new_data=data, server=server)
 
     # Given a name, the client retrieves associated data by calling:
     def retrieve_data(self, server, id, data):
         # the client returns None if the data does not exist.
-        return self.access(id)
+        return self.access(id, server=server)
 
     # The client deletes data associated with an ID from server by calling:
     def delete_data(self, server, id, data):
-        self.access(id, delete=True)
+        self.access(id, delete=True, server=server)
 
     # Access to server to retrieve data.
     # We pass the desired block_id and the new_data we want to update to it, if so.
     # The Function returns the read data while also updating a new path to the tree inside
     # the server. We can also delete data from a block using the "delete" argument.
-    def access(self, block_id, new_data=None, delete=False):
+    def access(self, block_id, new_data=None, delete=False, server=None):
+
+        if server is None:
+            server = self.server
+            self.key = server.share_key()
+            self.initialize_tree(server=server)
+
         # Handling non-existing blocks
         if block_id not in self.position_map:
             return None
 
         leaf = self.position_map[block_id]
-        path = self.get_path_to_leaf(leaf)
+        path = self.get_path_to_leaf(leaf, server=server)
 
-        self.read_path_to_stash(path)
-        data = self.find_and_update_block_in_stash(block_id, new_data, delete)
-        self.write_new_path_to_server(block_id, path, delete)
+        self.read_path_to_stash(path, server=server)
+        data = self.find_and_update_block_in_stash(block_id, new_data, delete, server=server)
+        self.write_new_path_to_server(block_id, path, delete, server=server)
 
+        # Back to default
+        self.key = self.server.share_key()
         return data
 
     # Get the shared key from Server
     def get_shared_key(self, server):
-        self.key = self.server.share_key()
+        self.key = server.share_key()
 
     # Encryption of a block
     def encrypt(self, data):
@@ -74,18 +82,22 @@ class Client():
         return hmac.compare_digest(h.digest(), mac)
 
     # initialize the Storage for the Server
-    def initialize_tree(self):
+    def initialize_tree(self, server=None):
+        if server is None:
+            server = self.server
+            self.key = server.share_key()
+
         for i in range(self.N):
             leaf = self.position_map[i]
-            path = self.get_path_to_leaf(leaf)
+            path = self.get_path_to_leaf(leaf, server)
             encrypted_data = self.encrypt(f"NULL")
             mac = self.generate_mac(encrypted_data)
 
             # Try to place the block in the leaf node first
             placed = False
             for bucket_id in reversed(path):
-                if self.server.get_bucket_size(bucket_id) < self.server.bucket_size:
-                    self.server.add_to_bucket(bucket_id, {'id': i, 'data': encrypted_data, 'valid': 0, 'mac': mac})
+                if server.get_bucket_size(bucket_id) < server.bucket_size:
+                    server.add_to_bucket(bucket_id, {'id': i, 'data': encrypted_data, 'valid': 0, 'mac': mac})
                     placed = True
                     break
 
@@ -95,9 +107,9 @@ class Client():
 
     # Fine the indexes path inside a binary tree on a way to a leaf
     # To use on storage inside the server
-    def get_path_to_leaf(self, leaf):
+    def get_path_to_leaf(self, leaf, server):
         path = []
-        idx = leaf + self.server.num_of_buckets // 2
+        idx = leaf + server.num_of_buckets // 2
         while idx >= 0:
             path.append(idx)
             if idx == 0:
@@ -106,57 +118,68 @@ class Client():
         return path[::-1]
 
     # Reading all blocks from the path into the stash
-    def read_path_to_stash(self, path):
-        path_data = self.server.read_path(path)
+    def read_path_to_stash(self, path, server):
+        path_data = server.read_path(path)
         self.stash.extend(path_data)
 
     # Finding the block with block_id in the stash.
     # In case we set the new_data argument, we shall update this
     # block to contain the new data
     # The function returns the "old" data from the block
-    def find_and_update_block_in_stash(self, block_id, new_data, delete):
+    def find_and_update_block_in_stash(self, block_id, new_data, delete, server):
         data = None
-        block_to_remove = None
+
         for block in self.stash:
             if block['id'] == block_id:
+
+                # Authenticity check
                 if not self.verify_mac(block['data'], block['mac']):
                     print(f"MAC verification failed for block {block_id}.")
                     return None
 
                 data = self.decrypt(block['data'])
                 if delete:
+                    # If we wish to delete the data in this block, we will return the data to "NULL"
                     block['data'] = self.encrypt('NULL')
                     block['valid'] = 0
                 elif new_data is not None:
+                    # If we wish to update the data in this block, we will change the data to to the desired value
                     encrypted_data = self.encrypt(new_data)
                     block['valid'] = 1
                     block['data'] = encrypted_data
                     block['mac'] = self.generate_mac(encrypted_data)
                 break
-
         return data
 
     # After re-assigning blocks to the path, write the new path
     # back to the server
-    def write_new_path_to_server(self, block_id, path, delete):
+    def write_new_path_to_server(self, block_id, path, delete, server):
+
         # Randomize new position map encoding
         if not delete:
-            # Update the position map
-            new_leaf = random.randint(0, 2 ** self.server.tree_height - 1)
+            new_leaf = random.randint(0, 2 ** server.tree_height - 1)
             self.position_map[block_id] = new_leaf
 
-        # Write the path back to server
-        # Also evict the blocks from the stash
         updated_path_data = []
+
+        # Iterate through each bucket ID in the path from the root to the leaf node.
         for bucket_id in path:
-            bucket = [block for block in self.stash if
-                      bucket_id in self.get_path_to_leaf(self.position_map[block['id']])]
-            updated_path_data.append(bucket[:self.server.bucket_size])
-            self.stash = [block for block in self.stash if block not in updated_path_data[-1]]
+            # For each Bucket ID along the path,
+            # select blocks from the stash that should be placed in this bucket:
+                # For each block inside the stash,
+                # Check if the current bucket ID is part of the path from root to the block.
+                # If so, add it do the updated data path.
+                bucket = [block for block in self.stash if
+                          bucket_id in self.get_path_to_leaf(self.position_map[block['id']], server)]
+                updated_path_data.append(bucket[:server.bucket_size])
+
+                # Evict the relevant blocks from the stash
+                self.stash = [block for block in self.stash if block not in updated_path_data[-1]]
 
         # Write updated data with MAC
         for block in updated_path_data:
             for bucket in block:
                 bucket['mac'] = self.generate_mac(bucket['data'])
 
-        self.server.write_path(path, updated_path_data)
+        # Write the new updated path back to server
+        server.write_path(path, updated_path_data)
